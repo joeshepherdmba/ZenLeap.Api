03ee4048a90df4fd0fe0eceedcaa6b2a53380d95
Joe Shepherd <joeshepherdmba@outlook.com>
2017-08-23 15:55:46 -0700

    foundation for authorization

--- ZenLeap.Api/Models/User.cs
+++ ZenLeap.Api/Models/User.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
+
 namespace ZenLeap.Api.Models
 {
-	public class User
-	{
+    public class User // TODO: wire up Identity
+    {
         [Key]
 		public int Id { get; set; }
         [Required]
@@ -14,9 +15,24 @@ namespace ZenLeap.Api.Models
         [Required]
         public string Email { get; set; }
         [Required]
-        public string Password { get; set; }
+        public string Password { get; set; } //TODO: Password hash
 
         public virtual ICollection<Project> Projects { get; set; }
         public virtual ICollection<ProjectTask> AssignedTasks { get; set; }
+
+		///// <summary>
+		///// Navigation property for the roles this user belongs to.
+		///// </summary>
+		//public virtual ICollection<TUserRole> Roles { get; } = new List<TUserRole>();
+
+		///// <summary>
+		///// Navigation property for the claims this user possesses.
+		///// </summary>
+		//public virtual ICollection<TUserClaim> Claims { get; } = new List<TUserClaim>();
+
+		///// <summary>
+		///// Navigation property for this users login accounts.
+		///// </summary>
+		//public virtual ICollection<TUserLogin> Logins { get; } = new List<TUserLogin>();
 	}
 }
--- ZenLeap.Api/Startup.cs
+++ ZenLeap.Api/Startup.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ZenLeap.Api.Data;
+using ZenLeap.Api.Models;
 
 namespace ZenLeap.Api
 {
@@ -23,7 +27,71 @@ namespace ZenLeap.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-        }
+
+			/*
+            // requires: using Microsoft.AspNetCore.Authorization;
+			//           using Microsoft.AspNetCore.Mvc.Authorization;
+			services.AddMvc(config =>
+			{
+				var policy = new AuthorizationPolicyBuilder()
+								 .RequireAuthenticatedUser()
+								 .Build();
+				config.Filters.Add(new AuthorizeFilter(policy));
+			});
+            */
+
+            // Identoty 2.0
+			services.AddIdentity<User, IdentityRole>()
+        		.AddEntityFrameworkStores<DataContext>()
+        		.AddDefaultTokenProviders();
+
+
+			// If you want to tweak Identity cookies, they're no longer part of IdentityOptions.
+			services.ConfigureApplicationCookie(options => options.LoginPath = "/Account/LogIn");
+			
+            // Facebook Authentication
+            services.AddAuthentication()
+					.AddFacebook(options => {
+						options.AppId = Configuration["auth:facebook:appid"];
+						options.AppSecret = Configuration["auth:facebook:appsecret"];
+					});
+
+			// Google Authentication
+			services.AddAuthentication()
+            		.AddGoogle(options => {
+            			options.ClientId = Configuration["auth:google:clientid"];
+            			options.ClientSecret = Configuration["auth:google:clientsecret"];
+            		});
+
+			// Microsoft Authentication
+			services.AddAuthentication()
+            		.AddMicrosoftAccount(options => {
+            			options.ClientId = Configuration["auth:microsoft:clientid"];
+            			options.ClientSecret = Configuration["auth:microsoft:clientsecret"];
+            		});
+
+			// Configure Identity
+			services.Configure<IdentityOptions>(options =>
+			{
+				// Password settings
+				options.Password.RequireDigit = true;
+				options.Password.RequiredLength = 8;
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequireUppercase = true;
+				options.Password.RequireLowercase = false;
+
+				// Lockout settings
+				options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+				options.Lockout.MaxFailedAccessAttempts = 10;
+
+				// User settings
+				options.User.RequireUniqueEmail = true;
+			});
+
+			// Add application services.
+			//services.AddTransient<IEmailSender, AuthMessageSender>();
+			//services.AddTransient<ISmsSender, AuthMessageSender>();
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -39,8 +107,10 @@ namespace ZenLeap.Api
 
             app.UseStaticFiles();
 
-			// DataContext context = new DataContext();
-			// DbInitializer.Initialize(context);
+            // DataContext context = new DataContext();
+            // DbInitializer.Initialize(context);
+
+            app.UseAuthentication();
 
 			app.UseMvc(routes =>
             {
@@ -51,6 +121,7 @@ namespace ZenLeap.Api
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
             });
+
         }
     }
 }
